4250e62725dbf35f779bbd339ab6b2eef83b67d7
mrs.sakshi <133587092+sakshikharchan@users.noreply.github.com>
2025-04-05 22:04:34 +0530

    save home heading
    by sakshi

--- UiProject/src/Component/Home.jsx
+++ UiProject/src/Component/Home.jsx
@@ -4,11 +4,12 @@ import Men from './Men'
 
 export default function Home() {
   return (
-   <>
-    {/* <h1>I am Home componet</h1> */}
-    <h1>Hello i am pooja from mumbai</h1>
-    <Carozol/>
-    <Men/>
-   </>
+    <>
+      {/* <h1>I am Home componet</h1> */}
+      <h1>Hello i am pooja from mumbai</h1>
+      <h1>Hello world</h1>
+      <Carozol />
+      <Men />
+    </>
   )
 }
