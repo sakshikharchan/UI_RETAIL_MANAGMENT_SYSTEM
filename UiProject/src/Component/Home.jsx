a920273db6ee07f079a7e0df4bf6b6cd9dd63ede
mrs.sakshi <133587092+sakshikharchan@users.noreply.github.com>
2025-04-05 22:37:48 +0530

    code done
    Women code by sakshi

--- UiProject/src/Component/Home.jsx
+++ UiProject/src/Component/Home.jsx
@@ -1,15 +1,20 @@
 import React from 'react'
 import Carozol from './carozol'
 import Men from './Men'
+import Women from './Women'
 
 export default function Home() {
   return (
     <>
       {/* <h1>I am Home componet</h1> */}
-      <h1>Hello i am pooja from mumbai</h1>
-      <h1>Hello world</h1>
+
+
       <Carozol />
       <Men />
+      <br></br>
+      <Women />
+
+
     </>
   )
 }
---
+++ UiProject/src/Component/Women.jsx
@@ -0,0 +1,76 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import Banner from '../assets/fash1.jpg';
+
+import '../App.css';
+let Women = () => {
+
+
+    return (<>
+
+        <div className="card" style={{ width: "18rem" }}>
+            <img src={Banner} className="card-img-top" alt="..." />
+            <div className="card-body">
+                <h5 className="card-title">Card title</h5>
+                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            </div>
+            <ul className="list-group list-group-flush">
+                <li className="list-group-item">An item</li>
+                <li className="list-group-item">A second item</li>
+                <li className="list-group-item">A third item</li>
+            </ul>
+            <div className="card-body">
+                <a href="#" className="card-link">Card link</a>
+                <a href="#" className="card-link">Another link</a>
+
+            </div>
+        </div>
+
+
+
+        {/* card 2 */}
+        <div className="card" style={{ width: "18rem" }}>
+            <img src={Banner} className="card-img-top" alt="..." />
+            <div className="card-body">
+                <h5 className="card-title">Card title</h5>
+                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            </div>
+            <ul className="list-group list-group-flush">
+                <li className="list-group-item">An item</li>
+                <li className="list-group-item">A second item</li>
+                <li className="list-group-item">A third item</li>
+            </ul>
+            <div className="card-body">
+                <a href="#" className="card-link">Card link</a>
+                <a href="#" className="card-link">Another link</a>
+
+            </div>
+        </div>
+
+        {/* card 3 */}
+        <div className="card" style={{ width: "18rem" }}>
+            <img src={Banner} className="card-img-top" alt="..." />
+            <div className="card-body">
+                <h5 className="card-title">Card title</h5>
+                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            </div>
+            <ul className="list-group list-group-flush">
+                <li className="list-group-item">An item</li>
+                <li className="list-group-item">A second item</li>
+                <li className="list-group-item">A third item</li>
+            </ul>
+            <div className="card-body">
+                <a href="#" className="card-link">Card link</a>
+                <a href="#" className="card-link">Another link</a>
+
+            </div>
+
+
+        </div>
+
+
+    </>);
+
+}
+export default Women;
