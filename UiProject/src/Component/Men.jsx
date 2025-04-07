c4d82e38ed6936f365965405c76868391d68358f
mrs.sakshi <133587092+sakshikharchan@users.noreply.github.com>
2025-04-07 09:12:33 +0530

    Men.jsx
    Rename Para

--- UiProject/src/Component/Men.jsx
+++ UiProject/src/Component/Men.jsx
@@ -47,7 +47,7 @@ let Men = () => {
             <img src={Banner2} className="card-img-top" alt="..." />
             <div className="card-body">
                 <h5 className="card-title">Card title</h5>
-                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's contents.</p>
             </div>
             <ul className="list-group list-group-flush">
                 <li className="list-group-item">An item</li>
